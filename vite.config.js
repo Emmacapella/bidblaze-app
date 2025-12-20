413a5f12cfee750e86f2e016462561cc0c82edbf
Emmacapella <your_real_email@gmail.com>
2025-12-20 07:55:54 +0100

    Fix Vercel build error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // This fixes the Web3 build error
+    commonjsOptions: {
+      transformMixedEsModules: true, // Helps with wallet libraries
+    },
+  },
 })
+
